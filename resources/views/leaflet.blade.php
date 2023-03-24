72a44b9653fee0bef2445c029f011910efd93f8b
Krisna Dana <krisnadanaofficial@gmail.com>
2023-03-24 09:45:49 +0800

    fixbugs

--- resources/views/leaflet.blade.php
+++ resources/views/leaflet.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <script src="https://kit.fontawesome.com/a94f3fd771.js" crossorigin="anonymous"></script>
         <link rel="icon" href="{{url('/images/hotel.png')}}" type="image/png" />
         <link rel="stylesheet" href="{{url('/css/bootstrap.min.css')}}" />
